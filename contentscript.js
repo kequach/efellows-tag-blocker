c268b8b111f3aa2bad6930ddb158fbe1d9d77176
Max Klinger <max.klinger@nextcrest.de>
2022-04-15 17:08:37 +0200

    Adding a sanity check that we are a) actually on E-Fellows and b) in one of the thread overviews

--- contentscript.js
+++ contentscript.js
@@ -2,11 +2,20 @@ var tags = document.getElementsByClassName('tag');
 blocklist = ["jura"]
 
 counter = 0;
-for (var i=0; i < tags.length; i++) {
-    if(blocklist.includes(tags[i].textContent.toLowerCase())) {
-        console.log(`Matched tag: ${tags[i].textContent}`);
-        tags[i].closest(".questionbox").remove()
-        counter ++;
-    }
+// Sicherstellen, dass wir in den EF Thread übersichten sind
+// Deckt alles von Neuste bis Beliebteste Fragen ab
+// Meine Beiträge haben eine andere URL und das ist gut so.
+taburl = document.location.href;
+console.log(`Scanning the following url for named tags ${taburl}`);
+
+if (taburl.includes("community.e-fellows.net/qanda/index/list")){
+		for (var i=0; i < tags.length; i++) {
+				if(blocklist.includes(tags[i].textContent.toLowerCase())) {
+						console.log(`Matched tag: ${tags[i].textContent}`);
+						tags[i].closest(".questionbox").remove()
+						counter ++;
+				}
+		}
 }
+
 console.log(`Removed ${counter} questions`);
